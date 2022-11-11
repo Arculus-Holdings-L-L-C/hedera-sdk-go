42e02336e6aa30f08fb1d5a0a1ec3be442540710
Carl Cherry <ccherry@arculus.co>
2022-11-11 10:57:41 -0500

    Remove panic from AddressBookQuery

--- address_book_query.go
+++ address_book_query.go
@@ -110,6 +110,7 @@ func (query *AddressBookQuery) _Build() *mirror.AddressBookQuery {
 func (query *AddressBookQuery) Execute(client *Client) (NodeAddressBook, error) {
 	var cancel func()
 	var ctx context.Context
+	var subClientError error
 
 	err := query._ValidateNetworkOnIDs(client)
 	if err != nil {
@@ -142,12 +143,14 @@ func (query *AddressBookQuery) Execute(client *Client) (NodeAddressBook, error) 
 						time.Sleep(time.Duration(delay) * time.Millisecond)
 						query.attempt++
 					} else {
-						panic(grpcErr.Err())
+						subClientError = grpcErr.Err()
+						break
 					}
 				} else if err == io.EOF {
 					break
 				} else {
-					panic(err)
+					subClientError = err
+					break
 				}
 			}
 
@@ -185,5 +188,5 @@ func (query *AddressBookQuery) Execute(client *Client) (NodeAddressBook, error) 
 
 	return NodeAddressBook{
 		NodeAddresses: result,
-	}, nil
+	}, subClientError
 }
